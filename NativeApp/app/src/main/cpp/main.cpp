218568d0aa8be6597bfbb5bcd32582233ffe98ad
Raymond Chiu <chiur@google.com>
2018-04-16 16:45:52 -0700

    Update libagbench.so and main.cpp

--- NativeApp/app/src/main/cpp/main.cpp
+++ NativeApp/app/src/main/cpp/main.cpp
@@ -246,16 +246,16 @@ static void engine_handle_cmd(struct android_app* app, int32_t cmd) {
             engine_term_display(engine);
             break;
         case APP_CMD_GAINED_FOCUS:
-//            // When our app gains focus, we start monitoring the accelerometer.
-//            if (engine->accelerometerSensor != NULL) {
-//                ASensorEventQueue_enableSensor(engine->sensorEventQueue,
-//                                               engine->accelerometerSensor);
-//                // We'd like to get 60 events per second (in us).
-//                ASensorEventQueue_setEventRate(engine->sensorEventQueue,
-//                                               engine->accelerometerSensor,
-//                                               (1000L/60)*1000);
-//            }
-//            break;
+            // When our app gains focus, we start monitoring the accelerometer.
+            if (engine->accelerometerSensor != NULL) {
+                ASensorEventQueue_enableSensor(engine->sensorEventQueue,
+                                               engine->accelerometerSensor);
+                // We'd like to get 60 events per second (in us).
+                ASensorEventQueue_setEventRate(engine->sensorEventQueue,
+                                               engine->accelerometerSensor,
+                                               (1000L/60)*1000);
+            }
+            break;
         case APP_CMD_LOST_FOCUS:
             // When our app loses focus, we stop monitoring the accelerometer.
             // This is to avoid consuming battery while not being used.
@@ -349,34 +349,34 @@ void android_main(struct android_app* state) {
 
     while (1) {
         // Read all pending events.
-//        int ident;
+        int ident;
         int events;
         struct android_poll_source* source;
-//
-//        // If not animating, we will block forever waiting for events.
-//        // If animating, we loop until all events are read, then continue
-//        // to draw the next frame of animation.
-        while ((ALooper_pollAll(engine.animating ? 0 : -1, NULL, &events,
+
+        // If not animating, we will block forever waiting for events.
+        // If animating, we loop until all events are read, then continue
+        // to draw the next frame of animation.
+        while ((ident = ALooper_pollAll(engine.animating ? 0 : -1, NULL, &events,
                                       (void**)&source)) >= 0) {
 
             // Process this event.
             if (source != NULL) {
                 source->process(state, source);
             }
-//
-//            // If a sensor has data, process it now.
-//            if (ident == LOOPER_ID_USER) {
-//                if (engine.accelerometerSensor != NULL) {
-//                    ASensorEvent event;
-//                    while (ASensorEventQueue_getEvents(engine.sensorEventQueue,
-//                                                       &event, 1) > 0) {
+
+            // If a sensor has data, process it now.
+            if (ident == LOOPER_ID_USER) {
+                if (engine.accelerometerSensor != NULL) {
+                    ASensorEvent event;
+                    while (ASensorEventQueue_getEvents(engine.sensorEventQueue,
+                                                       &event, 1) > 0) {
 //                        LOGI("accelerometer: x=%f y=%f z=%f",
 //                             event.acceleration.x, event.acceleration.y,
 //                             event.acceleration.z);
-//                    }
-//                }
-//            }
-//
+                    }
+                }
+            }
+
             // Check if we are exiting.
             if (state->destroyRequested != 0) {
                 engine_term_display(&engine);
